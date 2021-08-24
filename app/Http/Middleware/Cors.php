83223439cb959ddf828bd7274170896c5473388f
Goodbyez <miradadesigual@gmail.com>
2021-08-24 19:40:50 +0200

    hot fix kernel cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,10 +19,10 @@ class Cors
         return $next($request)
 
         //Url a la que se le dará acceso en las peticiones
-      ->header("Access-Control-Allow-Origin", "https://geeksdatefront.herokuapp.com")
-      //Métodos que a los que se da acceso
-      ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-      //Headers de la petición
-      ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization"); 
+        ->header("Access-Control-Allow-Origin", "https://geeksdatefront.herokuapp.com")
+        //Métodos que a los que se da acceso
+        ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        //Headers de la petición
+        ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization, Accept"); 
     }
 }
